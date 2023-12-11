61b2d3323263a6ea31a0d7ccda71809c642b9976
Ignacio T <ignacio_m_16@hotmail.com>
2023-12-11 13:08:35 -0300

    TEsteo agregado

--- api/app/utils.py
+++ api/app/utils.py
@@ -6,19 +6,13 @@ from pandas import DataFrame
 import os 
 from io import BytesIO
 
-import pickle
-
-
 def get_model() -> Pipeline:
     model_path = os.environ.get('MODEL_PATH','model/model.pkl')
     with open(model_path, "rb") as fh:
         model = load(BytesIO(fh.read()))
-    print(model)
     return model
 
 def transform_to_dataframe(class_model: BaseModel) -> DataFrame:
-    print(class_model)
-    transition_dictionary = {key:[value] for key, value in class_model.dict().items()}
+    transition_dictionary = {key:[value] for key, value in class_model.model_dump().items()}
     data_frame = DataFrame(transition_dictionary)
-    print(data_frame)
     return data_frame
